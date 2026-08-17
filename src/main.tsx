1d426a388d878d892db1a307dbdc47d9223579d3
Dimario0 <prijnik@gmail.com>
2026-08-17 17:55:57 +0200

    Route IT/PL content guides through app shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,25 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, useLocation } from 'react-router-dom';
 import App from './App';
+import GuideLanding from './components/GuideLanding';
 import { SiteProvider } from './context/SiteContext';
 import { LanguageProvider } from './context/LanguageContext';
 import './index.css';
+
+const GUIDE_SLUGS = [
+  'come-arrivare-da-lamezia-terme-a-scalea',
+  'jak-dojechac-z-lamezia-terme-do-scalei',
+  'scalea-senza-auto',
+  'scalea-bez-samochodu',
+];
+
+const RouteAwareApp: React.FC = () => {
+  const { pathname } = useLocation();
+  const isGuideLanding = GUIDE_SLUGS.some((slug) => pathname.includes(`/${slug}`));
+  return isGuideLanding ? <GuideLanding /> : <App />;
+};
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -18,7 +32,7 @@ root.render(
     <BrowserRouter>
       <LanguageProvider>
         <SiteProvider>
-          <App />
+          <RouteAwareApp />
         </SiteProvider>
       </LanguageProvider>
     </BrowserRouter>
